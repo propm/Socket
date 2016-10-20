3f699c3f6515c1c057d3fa12dabd5d5a069278da
aridai (Pro.PM) <i151304@gm.ishikawa-nct.ac.jp>
2016-10-20 23:19:09 +0900

    C#でDLL関数をDllImportした。

---
+++ SocketManaged/NativeFunc.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SocketManaged
+{
+    internal static class NativeFunc
+    {
+        [DllImport("SocketClient.dll")]
+        internal static extern bool setup();
+
+        [DllImport("SocketClient.dll")]
+        internal static extern int update();
+
+        [DllImport("SocketClient.dll")]
+        internal static extern void close();
+    }
+}
