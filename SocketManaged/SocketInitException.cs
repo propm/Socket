74dec83293f87f46895789ccdf7a34b8feae612e
aridai (Pro.PM) <i151304@gm.ishikawa-nct.ac.jp>
2016-10-20 23:31:24 +0900

    SocketInitExceptionクラスを追加した。

---
+++ SocketManaged/SocketInitException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SocketManaged
+{
+    /// <summary>
+    /// ソケット通信の初期化時に発生する例外を提供します。
+    /// </summary>
+    public class SocketInitException : Exception
+    {
+        internal SocketInitException() : base("ソケット通信の初期化処理でエラーが発生しました。") { }
+    }
+}
